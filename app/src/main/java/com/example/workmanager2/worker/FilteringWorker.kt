2f3e024243272013aed364b279f5a5a42a25dd0c
maiphuocloi01 <maiphuocloi01@gmail.com>
2021-07-23 19:03:34 +0700

    first commit

---
+++ app/src/main/java/com/example/workmanager2/worker/FilteringWorker.kt
@@ -0,0 +1,23 @@
+package com.example.workmanager2.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class FilteringWorker(context: Context,params:WorkerParameters) : Worker(context,params) {
+
+
+    override fun doWork(): Result {
+        try {
+
+            for (i in 0 ..3000) {
+                Log.i("MYTAG", "Filtering $i")
+            }
+
+            return Result.success()
+        } catch (e:Exception){
+            return Result.failure()
+        }
+    }
+}
